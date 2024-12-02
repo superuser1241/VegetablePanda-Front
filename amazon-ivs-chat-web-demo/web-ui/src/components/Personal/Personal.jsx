b8ef8f27b9b923f68c264e732f494aa1bc7b9412
WIN-2FRPJVNJLFJ\User <Gnusnuy>
2024-12-02 18:32:24 +0900

    .

--- amazon-ivs-chat-web-demo/web-ui/src/components/Personal/Personal.jsx
+++ amazon-ivs-chat-web-demo/web-ui/src/components/Personal/Personal.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./Personal.css";
+import "./personal.css";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
