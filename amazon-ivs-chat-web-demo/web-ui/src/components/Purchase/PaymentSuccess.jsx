b92d22bd67826c2950122b87f3f01cda3c505ab8
iylee15 <iylee153@gmail.com>
2024-12-05 00:42:46 +0900

    2024.12.05 00:42 결제 완료 페이지 데이터 수신 확인 - 인영

--- amazon-ivs-chat-web-demo/web-ui/src/components/Purchase/PaymentSuccess.jsx
+++ amazon-ivs-chat-web-demo/web-ui/src/components/Purchase/PaymentSuccess.jsx
@@ -1,26 +1,39 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const PaymentSuccess = () => {
     const location = useLocation();
-    const { orderUid } = location.state?.orderUid;
+    const token = localStorage.getItem('token');
+
+    // const { orderUid } = location.state?.orderUid;
+    // const { orderNumber, items, totalAmount } = order;
+
+    const [orderInfo, setOrderInfo] = useState({
+        buySeq : 0,
+        buyDate : '',
+        state: 0,
+        totalPrice: 0,
+        orderUid: '',
+        userBuyDetails : []
+    });
 
     useEffect(() => {
         const fetchOrderInfo = async () => {
-            const token = localStorage.getItem('token');
-            console.log('orderUid');
-            console.log(orderUid);
+            // console.log('orderUid');
+            // console.log(orderUid);
 
             try {
-                const response = await axios.post('http://localhost:9001/shop/order', {orderUid:orderUid}, {
+                const response = await axios.post('http://localhost:9001/shop/order', null, { 
+                    params: {orderUid:'O202412042302157451'},
                     headers: {
                         'Authorization': `Bearer ${token}`
                     }
                 });
 
+                console.log('response.data');
                 console.log(response.data);
-
+                setOrderInfo(response.data);
 
             } catch (error) {
                 console.error('주문 정보 조회 실패:', error);
@@ -36,12 +49,13 @@ const PaymentSuccess = () => {
         <div>
             <h2>결제 완료</h2>
             주문번호 : 000000
+            구매날짜 : {orderInfo.buyDate}
             <hr />
             주문정보 - orderUid이용해서
             주문 상세품목
 
             결제 정보
-
+            
 
         </div>
     );
