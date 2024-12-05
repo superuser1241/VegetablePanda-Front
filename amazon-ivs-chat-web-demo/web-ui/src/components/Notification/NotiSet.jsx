8fdfaed4c83d9e3676861fb41aa5c40c63b57e06
kimjaeyou <unknown88455144@gmail.com>
2024-12-05 10:38:48 +0900

    24/12/05/10:38-메세지 노출 수정-재영

--- amazon-ivs-chat-web-demo/web-ui/src/components/Notification/NotiSet.jsx
+++ amazon-ivs-chat-web-demo/web-ui/src/components/Notification/NotiSet.jsx
@@ -86,6 +86,7 @@ const NotiSet = ({ onSetStreamingRoom }) => {
     return (
         <>
             {showMessage && (
+            <div className="overlay">
                 <div className="MessageContainer">
                     <div className="MessageContent">
                         <button onClick={handleHideMessages} className="CloseButton">
@@ -99,6 +100,7 @@ const NotiSet = ({ onSetStreamingRoom }) => {
                         )}
                     </div>
                 </div>
+            </div>
             )}
 
 
