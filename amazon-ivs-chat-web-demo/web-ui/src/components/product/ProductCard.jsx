5a73cd6e8f4e0c9ad8840dd29be9f36045a7e8e9
iylee <iylee153@gmail.com>
2024-12-06 14:03:11 +0900

    2024.12.06 14:02 상품 카드 제목 수정 - 인영

--- amazon-ivs-chat-web-demo/web-ui/src/components/product/ProductCard.jsx
+++ amazon-ivs-chat-web-demo/web-ui/src/components/product/ProductCard.jsx
@@ -13,7 +13,7 @@ const productImage = product.file && product.file.trim() !== "" ? product.file :
                 <Link to = {`/product/{${product.stockSeq}}`} state={{ product }} className='default-link product-name'>
                 <img src= {product.file ? product.file : 'https://placehold.co/200x200?text=vegetable'} alt={product.productName} />
                 <p>{product.farmerName}</p>
-                <h3>{product.content}</h3>
+                <h3>{product.productName}</h3>
                 <p className='product-price'>{product.price.toLocaleString()}원</p>
                 <hr />
                 <p className='product-count'>수량 {product.count}</p>
