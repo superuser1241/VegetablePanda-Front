300bc7c03d6b0d75e41bf2697c309536051e0a6a
iylee <iylee153@gmail.com>
2024-12-06 13:49:31 +0900

    2024.12.05 13:49 마이페이지 경매 내역 - 인영

--- amazon-ivs-chat-web-demo/web-ui/src/components/myPage/UserMyPage.jsx
+++ amazon-ivs-chat-web-demo/web-ui/src/components/myPage/UserMyPage.jsx
@@ -8,6 +8,7 @@ import "../../index.css";
 import logo from "../../image/기본이미지.png";
 import ReviewCommentList from "../ReviewComment/ReviewCommentList.jsx";
 import Point from "./Point.jsx";
+import UserAuctionHistory from "./UserAuctionHistory.jsx";
 const UserMyPage = () => {
   const token = localStorage.getItem("token");
   const [userId, setUserId] = useState("");
@@ -141,7 +142,7 @@ const UserMyPage = () => {
     try {
       setLoading1(true); // 로딩 시작
       const response = await axios.get(
-        `http://localhost:9001/myPage/auction/${userId}`
+        `http://localhost:9001/myPage/auction/result/2`
       ); // API 엔드포인트
       setAuctions(response.data); // 데이터 저장
     } catch (err) {
@@ -197,8 +198,6 @@ const UserMyPage = () => {
     }
   };
 
-  const handleCharge = async () => {
-  }
   const handlePhoneChange = (e) => {
     const value = e.target.value.replace(/[^0-9]/g, "").slice(0, 11);
 
@@ -490,9 +489,9 @@ const UserMyPage = () => {
                </thead>
                <tbody>
                  {auctions.map((auction, index) => (
-                   <tr key={auction.bidSeq}>
+                   <tr key={auction.buySeq}>
                      <td>{index + 1}</td> {/* 구독 번호 */}
-                     <td>{auction.content}</td> {/* 판매자명 */}
+                     <td>{auction.productName}</td> {/* 상품명 */}
                      <td>{auction.count}</td> {/* */}
                      <td>{auction.price}원</td> {/* 입찰할 금액 */}
                      <td>{auction.insertDate}</td> {/* 입찰한 날짜 */}
@@ -613,56 +612,55 @@ const UserMyPage = () => {
           )}
 
           {activeTab === "auction" && (
-            <div className="auction-history-display">
-              <h3>경매 참여 내역</h3>
-              {loading1 ? (
-                <div>로딩 중...</div>
-              ) : error ? (
-                <div className="error-message">{error}</div>
-              ) : auctions.length > 0 ? (
-                <table>
-                  <thead>
-                    <tr>
-                      <th>번호</th>
-                      <th>상품명</th>
-                      <th>수량</th>
-                      <th>입찰 금액</th>
-                      <th>참여 일자</th>
-                      <th>판매자명</th>
-                      <th>현재 상태</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {auctions.map((auction, index) => (
-                      <tr key={auction.bidSeq}>
-                        <td>{index + 1}</td> {/* 번호 */}
-                        <td>{auction.content}</td> {/* 상품명 */}
-                        <td>{auction.count}</td> {/* 수량*/}
-                        <td>{auction.price}원</td> {/* 입찰할 금액 */}
-                        <td>{auction.insertDate}</td> {/* 입찰한 날짜 */}
-                        <td>{auction.name}</td> {/* 판매자명 */}
-                        <td>
-                          {auction.status === 0
-                            ? "값 뭐넣어야해여?"
-                            : auction.status === 1
-                            ? "값 뭐넣어야해여?"
-                            : auction.status === 2
-                            ? "값 뭐넣어야해여?"
-                            : auction.status === 3
-                            ? "값 뭐넣어야해여?"
-                            : "값 뭐넣어야해여?"}
-                        </td>
-                        {/* 현재 상태 */}
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              ) : (
-                <div className="no-data-notification">
-                  경매 참여 내역이 없습니다.
-                </div>
-              )}
-            </div>
+            // <div className="auction-history-display">
+            //   <h3>경매 참여 내역</h3>
+            //   {loading1 ? (
+            //     <div>로딩 중...</div>
+            //   ) : error ? (
+            //     <div className="error-message">{error}</div>
+            //   ) : auctions.length > 0 ? (
+            //     <table className="auction-history-table-container">
+            //       <thead>
+            //         <tr>
+            //           <th>번호</th>
+            //           <th>상품명</th>
+            //           <th>수량</th>
+            //           <th>낙찰 금액</th>
+            //           <th>참여 일자</th>
+            //           <th>판매자명</th>
+            //         </tr>
+            //       </thead>
+            //       <tbody>
+            //         {auctions.map((auction, index) => {
+            //           // 날짜 포맷 변경 함수
+            //           const formatDate = (dateString) => {
+            //             const date = new Date(dateString);
+            //             const formattedDate = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+            //             const formattedTime = `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`;
+            //             return `${formattedDate} ${formattedTime}`;
+            //           };
+            //           return (
+            //           <tr key={auction.bidSeq}>
+            //             <td>{auction.buySeq}</td> {/* 경매번호 */}
+            //             <td>{auction.productName}</td> {/* 상품명 */}
+            //             <td>{auction.count}</td> {/* 수량*/}
+            //             <td>{auction.totalPrice}원</td> {/* 입찰할 금액 */}
+            //             <td>{formatDate(auction.insertDate)}</td> {/* 입찰한 날짜 */}
+            //             <td>{auction.name}</td> {/* 판매자명 */}
+
+            //             {/* 현재 상태 */}
+            //           </tr>
+            //           )
+            //           })}
+            //       </tbody>
+            //     </table>
+            //   ) : (
+            //     <div className="no-data-notification">
+            //       경매 참여 내역이 없습니다.
+            //     </div>
+            //   )}
+            // </div>
+            <UserAuctionHistory  auctions = {auctions} loading1 = {loading1} error = {error}/>
           )}
 
           {activeTab === "update" && (
