f7e6ee4512ec8a34adfca6372c2d1e8f188e04e3
iylee <iylee153@gmail.com>
2024-12-11 15:17:40 +0900

    2024.12.11 15:17 추천상품 정보 페이지 - 인영

--- amazon-ivs-chat-web-demo/web-ui/src/components/product/Recommend.jsx
+++ amazon-ivs-chat-web-demo/web-ui/src/components/product/Recommend.jsx
@@ -1,62 +1,89 @@
 import React from 'react';
 import './Recommend.css';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const Recommend = () => {
-
+    const navigate = useNavigate();
     const { stockSeq } = useParams();
+    const serverIp = process.env.REACT_APP_SERVER_IP;
+
+    const fetchProductInfo = async () => {
+        const token = localStorage.getItem('token');
+
+        try {
+            const response = await axios.post(
+                `${serverIp}/api/insertShopLike`,
+                {
+                    
+                },
+                {
+                    headers: { 
+                        'Authorization': `Bearer ${token}`,
+                    }
+                }
+            );
+            console.log(response.data.state);
+
+            if(response.data.state===true) {
+                console.log('상품 정보 가져오기 성공.');
+            } else {
+                console.log('상품 정보 가져오기 실패.');
+
+            }
+
+        } catch (err) {
+            console.error('상품 정보를 가져오지 못하였습니다 :', err);
+        }
+    }
+
+    const movePage = () => {
+        
+    }
 
     return (
         <div>
-            {/* 상품 이미지 */}
-            <div className="product-image">
-                <img
-                src = {'https://placehold.co/200x200?text=vegetable'}
-                alt = {`대체텍스트`}
-                />
-            </div>
 
-            {/* 상품 정보 */}
-            <div className="product-info">
-                <h1>text</h1>
-                <hr/>
-                <div className='product-specs'>
-                    <div className='spec-item'>
-                        <span className='spec-label'>상품번호 </span>
-                        <span className='spec-value'>text</span>
-                    </div>
-                    <div className='spec-item'>
-                        <span className='spec-label'>카테고리 </span>
-                        <span className='spec-value'>text</span>
-                    </div>
-                    <div className="spec-item">
-                        <span className="spec-label">등급</span>
-                        <span className="spec-value">text</span>
-                    </div>
-                    <div className="spec-item">
-                        <span className="spec-label">인증</span>
-                        <span className="spec-value">dd</span>
-                    </div>
-                    <div className="spec-item">
-                        <span className="spec-label">재고</span>
-                        <span className="spec-value">90개</span>
-                    </div>
-                    <div className="spec-item">
-                        <span className="spec-label">판매가격</span>
-                        <span className="spec-value">9000원</span>
-                    </div>    
-                    <div className="spec-item">
-                        <span className="spec-label">배송비</span>
-                        <span className="spec-value">3,000원</span>
-                    </div>    
+            <div className='product-container'>
+                <div className='product-category'>
+                    
                 </div>
-                <div className="purchase-section">
-                    <div className="quantity-selector">
-                        <label>구매 수량</label>
-                        <div className="quantity-input-group">
+                <div className="product-details">
+
+                    {/* 상품 이미지 */}
+                    <div className="product-image">
+                        <img
+                        src = {'https://placehold.co/200x200?text=vegetable'}
+                        alt = {`대체텍스트`}
+                        />
+                    </div>
+
+                    {/* 상품 정보 */}
+                    <div className="product-info">
+                        <h1>stockSeq로 가져온 상품명</h1>
+                        <hr/>
+                        <div className='product-specs'>
+                            <div className='spec-item'>
+                                <span className='spec-label'>경매상태 </span>
+                                <span className='spec-value'>경매 상태 표기</span>
+                            </div>
+                            <div className='spec-item'>
+                                <span className='spec-label'>일반상점 상태 표기 </span>
+                                <span className='spec-value'>text</span>
+                            </div>
+                            <div className="spec-item">
+                                <span className="spec-label">가격</span>
+                                <span className="spec-value">경매중인 상품이면 가격 표기x</span>
+                            </div>
+                            <div className="spec-item">
+                                <span className="spec-label">재고</span>
+                                <span className="spec-value">90개</span>
+                            </div>
                         </div>
+                        <button className="product-buy-button" onClick={movePage}>이동</button>
                     </div>
                 </div>
+                
             </div>
         </div>
     );
