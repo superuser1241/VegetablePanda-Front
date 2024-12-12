1b20f00b2cdc39ebff531521713348f785bb3087
kimjaeyou <unknown88455144@gmail.com>
2024-12-12 09:19:02 +0900

    24/12/12/09:18--작업재영

--- amazon-ivs-chat-web-demo/web-ui/src/components/auction/BiddingSection.jsx
+++ amazon-ivs-chat-web-demo/web-ui/src/components/auction/BiddingSection.jsx
@@ -43,7 +43,7 @@ const BiddingSection = memo(({
     useEffect(() => {
         const serverIp = process.env.REACT_APP_SERVER_IP;
         const client = new Client({
-            brokerURL: `wss://${serverIp.replace('https://', '')}/ws`,
+            brokerURL: `ws://${serverIp.replace('https://', '')}/ws`,
             onConnect: () => {
                 client.subscribe("/top/notifications", async (message) => {
                     refreshAuctionData();
